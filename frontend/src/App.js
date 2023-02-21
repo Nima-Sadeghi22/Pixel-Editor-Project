0670654452b0004527f41e3dcf14ac60104eda5d
Andrea Hayes <amock516@gmail.com>
2023-02-20 21:28:21 -0800

    scaffold signature pad

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import SignaturePad from './SignaturePad';
 function App() {
   const [currentTime, setCurrentTime] = useState(0);
   const [openModel, setOpenModal] = useState(false);
+  const [imageUrl, setImageURL] = useState('https://camo.githubusercontent.com/f4e79a4215204104c0c1945e5cb02676fc6f42236321c20e186de3a66f4528b0/68747470733a2f2f6e6f6465692e636f2f6e706d2f72656163742d7369676e61747572652d63616e7661732e706e673f646f776e6c6f6164733d7472756526646f776e6c6f616452616e6b3d747275652673746172733d74727565');
 
   useEffect(() => {
     fetch('http://127.0.0.1:5000/').then(res => res.json()).then(data => {
@@ -21,6 +22,7 @@ function App() {
 
         <p> {currentTime}.</p>
         <p><button onClick={() => setOpenModal(true)}>Create Signature</button></p>
+        
       </header>
 
       {openModel && (
@@ -30,6 +32,7 @@ function App() {
             <div className="modal__bottom">
               <button onClick={() => setOpenModal(false)}>Cancel</button>
             </div>
+
           </div>
         </div>
       )}
--- frontend/src/SignaturePad.js
+++ frontend/src/SignaturePad.js
@@ -1,14 +1,58 @@
-import React, { useState } from "react";
+import React, { useState, useRef,Fragment } from "react";
 import SignatureCanvas from 'react-signature-canvas'
 
 function SignaturePad(){
 
 const [openModel, setOpenModal] = useState(false);
+const sigCanvas = useRef();
+const [penColor, setPenColor] = useState("black");
+const colors = ["black", "green", "red", "purple", "yellow", "blue", "pink", "orange"]
+const [imageURL, setImageURL] = useState(null);
+
+
+const create = () => {
+  const URL = sigCanvas.current.getTrimmedCanvas().toDataURL("image/png");
+  setImageURL(URL);
+  setOpenModal(false);
+};
+  // {
+  //   imageUrl && (
+  //     <>
+  //       <img src={imageUrl} alt="signature" className="signature" />
+  //     </>
+  //   )
+  // }
+
+
   //Pad to Draw Signature
   return (
-    <div className="sigPadContainer">
-      <SignatureCanvas penColor="black" canvasProps={{ className: "sigCanvas" }} />
-    </div>
+    <Fragment>
+      <div className="sigPad__penColors">
+        <p>Pen Color:</p>
+        {colors.map((color) => (
+          <span
+          key={color}
+          style={{
+            backgroundColor: color,
+            border: `${color === penColor ? `2px solid ${color}` : ""}`,
+          }}
+          onClick={() => setPenColor(color)}
+          ></span>
+        ))}
+      </div>
+
+      <div className="sigPadContainer">
+        <SignatureCanvas penColor={penColor}
+        canvasProps={{ className: "sigCanvas" }} 
+        ref={sigCanvas}
+        />
+        <hr/>
+        <button onClick={() => sigCanvas.current.clear()}>Clear</button>
+        <button className="create" onClick={create}>
+              Create
+        </button>
+      </div>
+    </Fragment>
   );
  }
 
