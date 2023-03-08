8f0799ebc01ae68ad8e9a32d581225e1c52e7c80
Mark Lee <cmlee1324@g.ucla.edu>
2023-03-08 01:30:08 -0800

    Refactoring pixelEditorScreen.js

--- frontend/src/pixelEditorScreen.js
+++ frontend/src/pixelEditorScreen.js
@@ -1,20 +1,36 @@
 import React, { useRef, useEffect } from 'react'
 import "./pixelStyles.css"
 
-function PixelEditor(props){
-    let x=props.x;
-    let y=props.y;
-    let pixelWidth=props.pixelWidth;
-    let gridWidth=props.gridWidth;
-    let gridHeight=props.gridHeight;
-    let defaultColor=props.defaultColor;
-    let selectedColor=props.selectedColor;
-    let screenOffset = 5; // 5 is the temporary offset before I fix the blur issue
+    var x;
+    var y;
+    var pixelWidth;
+    var gridWidth;
+    var gridHeight;
+    var defaultColor;
+    var selectedColor;
+    var screenOffset;
 
-    var screenWidth = (x+gridWidth*pixelWidth + screenOffset);  
-    var screenHeight = (y+gridHeight*pixelWidth + screenOffset);
+    var screenWidth; 
+    var screenHeight;
 
     var gridColors = [];
+
+function PixelEditor(props){
+
+    x=props.x;
+    y=props.y;
+    pixelWidth=props.pixelWidth;
+    gridWidth=props.gridWidth;
+    gridHeight=props.gridHeight;
+    defaultColor=props.defaultColor;
+    selectedColor=props.selectedColor;
+    screenOffset = 5; // 5 is the temporary offset before I fix the blur issue
+
+    screenWidth = (x+gridWidth*pixelWidth + screenOffset);  
+    screenHeight = (y+gridHeight*pixelWidth + screenOffset);
+
+    gridColors = [];
+    
     for (let i = 0; i < gridWidth; i++) {
         gridColors.push([]);
         for (let j = 0; j < gridHeight; j++) {
@@ -39,47 +55,6 @@ function PixelEditor(props){
     //--------------------------------------
 
     const canvasRef = useRef(null)
-
-    function drawGrid(ctx, init){
-        if(init){
-            ctx.canvas.width = screenWidth;
-            ctx.canvas.height = screenHeight;
-            ctx.lineWidth = 1.3;
-        }
-        ctx.strokeStyle = '#000000';
-        ctx.beginPath();
-        for (let i = 0; i < gridWidth; i++) {
-			for (let j = 0; j < gridHeight; j++) {
-				ctx.fillStyle = gridColors[i][j];
-				ctx.fillRect(i * pixelWidth + x, j * pixelWidth + y, pixelWidth, pixelWidth);
-			}
-		}
-        for(let i = 0; i <= gridHeight; i++){
-            ctx.moveTo(x-1, pixelWidth*i+y);                        //-1 and +1 create 
-            ctx.lineTo(gridWidth*pixelWidth+x+1, pixelWidth*i+y);   //square corners 
-        }
-        for(let i = 0; i <= gridWidth; i++ ){
-            ctx.moveTo(pixelWidth*i+x, y);
-            ctx.lineTo(pixelWidth*i+x, gridHeight*pixelWidth+y);
-        }
-        ctx.stroke();
-        ctx.closePath();
-    }
-
-    function onClick(canvas, event) {
-        const rect = canvas.getBoundingClientRect()
-        const mousex = event.clientX - rect.left
-        const mousey = event.clientY - rect.top
-        let xindex = (mousex-x)/pixelWidth, yindex = (mousey-y)/pixelWidth
-        xindex = Math.floor(xindex)
-        yindex = Math.floor(yindex)
-        if(xindex<0||yindex<0||xindex>gridWidth-1||yindex>gridHeight-1){
-            return
-        }
-        gridColors[xindex][yindex] = selectedColor;
-        console.log(xindex+", "+yindex+", "+gridColors[xindex][yindex])
-        drawGrid(canvas.getContext('2d'), false);
-    }
     
     useEffect(() => {
         const canvas = canvasRef.current
@@ -91,14 +66,51 @@ function PixelEditor(props){
             })
         }
     })
-    
     return (
         <div>
             <canvas ref={canvasRef} {...props}/>
         </div>
-    
     );
-    
+}
+
+function drawGrid(ctx, init){
+    if(init){
+        ctx.canvas.width = screenWidth;
+        ctx.canvas.height = screenHeight;
+        ctx.lineWidth = 1.3;
+    }
+    ctx.strokeStyle = '#000000';
+    ctx.beginPath();
+    for (let i = 0; i < gridWidth; i++) {
+        for (let j = 0; j < gridHeight; j++) {
+            ctx.fillStyle = gridColors[i][j];
+            ctx.fillRect(i * pixelWidth + x, j * pixelWidth + y, pixelWidth, pixelWidth);
+        }
+    }
+    for(let i = 0; i <= gridHeight; i++){
+        ctx.moveTo(x-1, pixelWidth*i+y);                        //-1 and +1 create 
+        ctx.lineTo(gridWidth*pixelWidth+x+1, pixelWidth*i+y);   //square corners 
+    }
+    for(let i = 0; i <= gridWidth; i++ ){
+        ctx.moveTo(pixelWidth*i+x, y);
+        ctx.lineTo(pixelWidth*i+x, gridHeight*pixelWidth+y);
+    }
+    ctx.stroke();
+    ctx.closePath();
+}
+
+function onClick(canvas, event) {
+    const rect = canvas.getBoundingClientRect()
+    const mousex = event.clientX - rect.left
+    const mousey = event.clientY - rect.top
+    let xindex = (mousex-x)/pixelWidth, yindex = (mousey-y)/pixelWidth
+    xindex = Math.floor(xindex)
+    yindex = Math.floor(yindex)
+    if(xindex<0||yindex<0||xindex>gridWidth-1||yindex>gridHeight-1){
+        return
+    }
+    gridColors[xindex][yindex] = selectedColor;
+    drawGrid(canvas.getContext('2d'), false);
 }
 
 export default PixelEditor
