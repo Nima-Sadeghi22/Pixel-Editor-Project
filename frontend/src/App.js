612c40089ff0755e396778bc748c0236a508dc19
Nima Sadeghi <nimas2000@g.ucla.edu>
2023-03-13 17:02:34 -0700

    master is now updated with Nima and Chris branches

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import Header from './components/Header'
 import useToken from './components/useToken'
 import Home from './components/Home'
 import Navbar from './components/Navbar';
+import PixelEditor from './pixelEditorScreen';
 import './App.css'
 
 function App() {
@@ -27,6 +28,8 @@ function App() {
             <Routes>
               <Route exact path="http://127.0.0.1:5000/profile" element={<Profile token={token} setToken={setToken}/>}></Route>
               <Route exact path = "/profile" element = {<Profile></Profile>}></Route>
+              <Route exact path = "/pixeleditor" element = {<PixelEditor x={22} y={22} gridWidth={22} gridHeight={22} pixelWidth={24} defaultColor="#FFFF00"
+          selectedColor="#63C5DA"></PixelEditor>}></Route>
               
             </Routes>
           
