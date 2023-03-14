5a55cd61ad998183ebc4347ddff5d2fe5a7f275c
bzhao567 <123792290+br-zhao@users.noreply.github.com>
2023-03-14 09:58:31 -0700

    added visual enhancements for the search bar
    search icon, expands on focus, changed font, icon animation when hovering, box shadow on focus

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import SearchBar from './Searchbar';
-import Forum from './forum';
+
 
 function App() {
   const [currentTime, setCurrentTime] = useState(0);
 
   useEffect(() => {
-    fetch('/forum/post').then(res => res.json()).then(data => {
-      setCurrentTime(data.time);
-    });
   }, []);
 
   return (
     <div className="App">
       <header className="App-header">
         <SearchBar />
-      <Forum />
       </header>
     </div>
   );
